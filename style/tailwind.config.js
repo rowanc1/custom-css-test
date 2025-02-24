47e42901a16c5ddbc2c6dc9c4ca35f791f63801d
luukfroling <L.Froling@student.tudelft.nl>
2025-02-24 22:34:34 +0100

    update
    fs

--- style/tailwind.config.js
+++ style/tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
 	  // Include styles defined in the myst-theme package
 	  ...mystTheme.content,
 	  // Parse styles from our markup
-	  "../content/**/*.md"
+	  "../content/**/*.md",
+    "./index.css" // Ensure Tailwind processes this file
   ],
   theme: {
     extend: mystTheme.themeExtensions,
